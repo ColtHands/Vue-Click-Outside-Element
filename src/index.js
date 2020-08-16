197878a4665e19848a5f1dd1cf4cc43b6a47ebcf
F1ks3r <f1ks3r@yandex.ru>
2020-08-16 21:16:55 +0300

    error fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ module.exports = {
     install(Vue) {
         Vue.directive('click-outside-element', {
             bind(el, bind, vn) {
-                e.cO = event => {
+                el.cO = event => {
                     if (!(el == event.target || el.contains(event.target))) {
                         if(vn.context[bind.expression]) {
                             vn.context[bind.expression](event)
                         }
                     }
                 }
-                document.body.addEventListener('click', e.cO)
+                document.body.addEventListener('click', el.cO)
             },
             unbind(el) {
                 document.body.removeEventListener('click', el.cO)
