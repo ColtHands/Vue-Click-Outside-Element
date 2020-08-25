dcb82cad311778abde07d4dc7fc5bc1276b1180d
F1ks3r <f1ks3r@yandex.ru>
2020-08-25 12:56:43 +0300

    adding more ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { DirectiveOptions } from 'vue/types'
 
 interface ExtendedHTMLElement extends HTMLElement {
-    clickOutside: (event: any) => void;
+    clickOutside: (event: any) => void
 }
 
 const directive:DirectiveOptions = {
     bind(el, bind, vn): void {
         const elem = el as ExtendedHTMLElement
+
         elem.clickOutside = (event) => {
             if (!(elem == event.target || elem.contains(event.target))) {
                 if(vn.context && vn.context[bind.expression]) {
@@ -16,12 +17,14 @@ const directive:DirectiveOptions = {
         }
         document.body.addEventListener('click', elem.clickOutside)
     },
-    unbind(el) {
+    unbind(el): void {
         const elem = el as ExtendedHTMLElement
+
         document.body.removeEventListener('click', elem.clickOutside)
     }
 }
 
+// TODO add Vue type, if it exists
 module.exports = {
     install(Vue: any): void {
         Vue.directive('click-outside-element', directive)
